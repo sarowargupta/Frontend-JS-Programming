89900c10cc15538f09999f89586857347f24505f
sarowargupta <sarowargupta2001@gmail.com>
2025-03-03 10:34:06 +0530

    [Sarowar Gupta]add. Add two random dice number.

---
+++ Day-01/SequencePracticeProblem/TwoRandomDiceNumber.js
@@ -0,0 +1,6 @@
+//Use REPL – Add two Random Dice Number and Print the Result
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+let sum = dice1 + dice2;
+
+console.log(`Dice 1: ${dice1}, Dice 2: ${dice2}, Sum: ${sum}`);
