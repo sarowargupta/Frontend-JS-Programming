6b39114ce635b4143abf32bd3cf5f8e20169c3a1
sarowargupta <sarowargupta2001@gmail.com>
2025-03-05 14:31:55 +0530

    [Sarowar Gupta]add. Read a Number and Display the week day (Sunday, Monday,...).

---
+++ Day-01/Problemswithcasestatements/DisplayWeekDay.js
@@ -0,0 +1,29 @@
+// Get command line argument
+const number = parseInt(process.argv[2], 10);
+
+// Convert number to weekday using switch-case
+switch (number) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Please enter a number between 1 and 7");
+}
